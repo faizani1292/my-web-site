5ec9f0ca81a31c6a01a6af3ed0427f5b94446f08
Faizan Ahmad <ahmadfai@corp.contoursoftware.net>
2022-12-18 19:21:07 +0500

    Javascript file modified to check for 10 characters added

--- palindrome.js
+++ palindrome.js
@@ -3,16 +3,19 @@ function palindrome(string) {
 
     // find the length of a string
     const len = string.length;
-
+    // check if string length is 10
+    if (len < 10) {
     // loop through half of the string
-    for (let i = 0; i < len / 2; i++) {
+        for (let i = 0; i < len / 2; i++) {
 
-        // check if first and last string are same
-        if (string[i] !== string[len - 1 - i]) {
-            return 'It is not a palindrome';
+            // check if first and last string are same
+            if (string[i] !== string[len - 1 - i]) {
+                return 'It is not a palindrome';
+            }
         }
+        return 'It is a palindrome';
     }
-    return 'It is a palindrome';
+    return 'String is greater than 10 characters';
 }
 
 // take input
